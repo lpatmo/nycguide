ae66128e8d1906eb2ba5dd42dd133892c3e39ad6
lpatmo <linda.peng@moveon.org>
2013-10-15 02:15:34 -0400

    updated about

--- js/map.js
+++ js/map.js
@@ -1,15 +1,20 @@
 var colorPrefix = 'http://maps.google.com/mapfiles/ms/icons/';
-var green = colorPrefix + 'green-dot.png';
-var blue = colorPrefix + 'blue-dot.png';
-var red = colorPrefix + 'red-dot.png';
+  var green = colorPrefix + 'green-dot.png';
+  var blue = colorPrefix + 'blue-dot.png';
+  var red = colorPrefix + 'red-dot.png';
 
-var locations = [
-      ['Alley NYC', 40.753694, -73.989082, green, '<h3>Alley NYC</h3>'],
-      ['General Assembly', 40.740575, -73.990112, blue, '<h3>General Assembly</h3>'],
+var content = [
+  ['<h3>Alley NYC</h3>'],
+  ['<h3>General Assembly</h3>' + '<p>Co-working space</p>'
+  ]
+]
+
+var places = [
+      ['Alley NYC', 40.753694, -73.989082, green, content[0]],
+      ['General Assembly', 40.740575, -73.990112, blue, content[1]],
       ['Trader Joe\'s in Brooklyn', 40.690271, -73.992669, red, 5]
     ];
 
-console.log(green);
 
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 13,
@@ -23,40 +28,40 @@ console.log(green);
 
     var descriptions = '';
 
-       for (i = 0; i < locations.length; i++) { 
-        console.log(locations[i][3]);
-      if (locations[i][3] === "green") {
+       for (i = 0; i < places.length; i++) { 
+        console.log(places[i][3]);
+      if (places[i][3] === "green") {
         markerColor = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
-           } else if (locations[i][3] === "blue" ) {
+           } else if (places[i][3] === "blue" ) {
         markerColor = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
            } else {
         markerColor = 'http://icons.iconarchive.com/icons/icons-land/vista-map-markers/256/Map-Marker-Ball-Azure-icon.png';
            }
          };
 
-    for (i = 0; i < locations.length; i++) { 
+    for (i = 0; i < places.length; i++) { 
       marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        position: new google.maps.LatLng(places[i][1], places[i][2]),
         map: map,
-        icon: locations[i][3]
+        icon: places[i][3]
       });
 
     
 
-     // $('#caption-container').append('<div class="hidden caption' + i + '">' + locations[i][3] + '</div>');
+     // $('#caption-container').append('<div class="hidden caption' + i + '">' + places[i][3] + '</div>');
      //if I don't want any of the caption-containers to disappear 
 
       google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
-          infowindow.setContent(locations[i][0]);
+          infowindow.setContent(places[i][0]);
           infowindow.open(map, marker);
           //$('.caption' + i).css('visibility','visible').fadeIn('slow').removeClass('hidden');
-         $('#description').hide().html(locations[i][4]).fadeIn();
+         $('#description').hide().html(places[i][4]).fadeIn();
         }
       })(marker, i));
 
-       //descriptions += '<div class="hidden caption' + i + '">' + locations[i][3] + '</div>';
-      //var descriptions = locations[i][3];
+       //descriptions += '<div class="hidden caption' + i + '">' + places[i][3] + '</div>';
+      //var descriptions = places[i][3];
       console.log(descriptions);
     }
 
