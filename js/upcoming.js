1c94173bbd75dd95bd2f0c9d71fc41fae71ef4d1
lpatmo <linda.peng@alumni.duke.edu>
2014-04-26 14:47:46 -0400

    made each of the event boxes toggle-able to show more description

--- js/upcoming.js
+++ js/upcoming.js
@@ -1,19 +1,33 @@
  $(document).ready(function(){
-    $.getJSON("http://api.meetup.com/2/open_events.json?zip=10001&radius=7&topic=technology&status=upcoming&time=,1w&key=82c3111664b49681a1755d782e442a&callback=?", 
-function (data) {
+    var promise = $.getJSON("http://api.meetup.com/2/open_events.json?zip=10001&radius=7&topic=technology&status=upcoming&time=,1w&key=82c3111664b49681a1755d782e442a&callback=?"); 
+
+promise.done(function (data) {
     var htmlString = "";
     $.each(data.results, function (i, item) {
-         if (item.venue) {
+         if (item.venue && item.description) {
             var time = item.time;
             var date = moment(time).format('dddd, MMMM Do YYYY [at] ha');
-        htmlString += '<span class="upcoming-block"><a href="' + item.event_url + '" target="_blank">' + item.name + '<span class="event-time">' + date + '</span></a>' + '</span>';
+        htmlString += '<div class="upcoming-block"><div class="expand" target="_blank">' + item.name + '<span class="event-time">' + date + '</span>' + '</div><div class="hidden-until-shown custom-hide">' + item.description + '</div>' + '</div>';
       //  var address_1 = item.venue['address_1'];
       // var venuetesting = item.venue;
       // console.log(venuetesting["id"]);
       //  console.log(item.venue["address_1"]);
-        }
-      
-    });
+
+      $('body').on('click', 'div.upcoming-block', function() {
+          // e.preventDefault();
+          $(this).find('.hidden-until-shown').toggleClass('custom-hide');
+           $('.upcoming-block:hover').css({'background': 'none', 'color': '#aaa'}); 
+           $('.upcoming-block:hover a').css({'color': '#aaa'}); 
+       });
+
+        }    //end if
+
+    }); //end each
+
+
     
-    $('#upcoming').html(htmlString);});
-    });
+    $('#upcoming').html(htmlString);
+  }); //end promise
+
+
+  }); //end document ready
